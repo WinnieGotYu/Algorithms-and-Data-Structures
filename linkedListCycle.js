592760e9db5fad04c99d5645ab49addf1f44bdc1
WinnieGotYu <WinnieGotYu@gmail.com>
2020-02-18 12:44:18 -0800

    updated test

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -51,7 +51,7 @@ let node3 = new ListNode(3);
 let node4 = new ListNode(4);
 let node5 = new ListNode(5);
 node1.next = node2;
-node2.next = node3; 
+node2.next = node3;
 node3.next = node4;
 node4.next = node2;
 
