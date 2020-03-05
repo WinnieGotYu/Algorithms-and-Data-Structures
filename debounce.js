daf2e222fef621e4ddabc5f747830453b8b4f0e3
WinnieGotYu <WinnieGotYu@gmail.com>
2020-03-04 16:06:22 -0800

    updated solution

--- debounce.js
+++ debounce.js
@@ -35,3 +35,25 @@ function debounce(fn, time){
 
   }
 }
+
+//solution: 
+  
+function debounce(fn, time) {
+  let timeoutId;
+
+  return function() {
+    // Check for existing calls
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      // Invoke fn
+      fn.apply(this, arguments);
+      // Clear timeout
+      timeoutId = null;
+
+    }, time);
+  }
+
+}
