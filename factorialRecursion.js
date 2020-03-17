b912679834a7127fda8b40de8349a6d468860fcd
WinnieGotYu <WinnieGotYu@gmail.com>
2020-03-17 16:45:56 -0700

    added factorialRecurison

---
+++ factorialRecursion.js
@@ -0,0 +1,12 @@
+/**write a function where it takes a number and returns its factorial
+ * 
+ * ie: factorial(5) = 120 
+ * 5 * 4 * 3 * 2 * 1
+ */
+
+function factorial(num){
+  if(num === 1) return 1; 
+  return num * factorial(num -1);
+}
+
+console.log(factorial(5)) //120;
