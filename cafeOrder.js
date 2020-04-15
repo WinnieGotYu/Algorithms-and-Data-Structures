45ec8bebc7eb5e709d1596ed28be3c0b96dc42a9
WinnieGotYu <WinnieGotYu@gmail.com>
2020-04-15 11:29:49 -0700

    added test

--- cafeOrder.js
+++ cafeOrder.js
@@ -1,7 +1,5 @@
 function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
-
   // Check if we're serving orders first-come, first-served
-  
   let t = takeOutOrders.length - 1; 
   let d = dineInOrders.length - 1; 
   
@@ -24,38 +22,38 @@ function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
 
 // Tests
 
-let desc = 'both registers have same number of orders';
+let desc = "both registers have same number of orders";
 let actual = isFirstComeFirstServed([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
 assertEquals(actual, true, desc);
 
-desc = 'registers have different lengths';
+desc = "registers have different lengths";
 actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]);
 assertEquals(actual, false, desc);
 
-desc = 'one register is empty';
+desc = "one register is empty";
 actual = isFirstComeFirstServed([], [2, 3, 6], [2, 3, 6]);
 assertEquals(actual, true, desc);
 
-desc = 'served orders is missing orders';
+desc = "served orders is missing orders";
 actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 6, 3, 5]);
 assertEquals(actual, false, desc);
 
-desc = 'served orders has extra orders';
+desc = "served orders has extra orders";
 actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]);
 assertEquals(actual, false, desc);
 
-desc = 'one register has extra orders';
+desc = "one register has extra orders";
 actual = isFirstComeFirstServed([1, 9], [7, 8], [1, 7, 8]);
 assertEquals(actual, false, desc);
 
-desc = 'one register has unserved orders';
+desc = "one register has unserved orders";
 actual = isFirstComeFirstServed([55, 9], [7, 8], [1, 7, 8, 9]);
 assertEquals(actual, false, desc);
 
-function assertEquals(a, b, desc) {
-  if (a === b) {
-    console.log(`${desc} ... PASS`);
+function assertEquals(a, b, desc){
+  if(a === b){
+    console.log(`"${desc}", Expected: ${a}, Output: ${b} ✅`)
   } else {
-    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    console.log(`"${desc}", Expected: ${a} Output: ${b} ❌`)
   }
 }
