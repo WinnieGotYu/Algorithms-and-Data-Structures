06bda5e39ce9fabae11d33b26a964a2784a75163
WinnieGotYu <WinnieGotYu@gmail.com>
2020-04-17 13:14:25 -0700

    changed sort function to ternary function

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -12,17 +12,14 @@ var merge = function (intervals) {
   // sort by first integer
   const sorted = intervalsCopy.sort((a, b) => {
     // -1 for a puts a's index lower than b
-    // 1 for b puts b's index lower than a 
-    // 0 keep the same 
-    if (a[0] === b[0]) {
-      return a[1] - b[1];
-    } else {
-      return a[0] - b[0];
-    }
+    // 1 for b puts b's index lower than a
+    // 0 keep the same
+    let result = a[0] === b[0] ? a[1] - b[1] : a[0] - b[0];
+    return result;
   });
   // initiate result variable
   const result = [sorted[0]];
-  
+
   // iterate through sorted copy of intervals
   for (let i = 0; i < sorted.length; i++) {
     // initiate current
@@ -51,15 +48,27 @@ let expect = [[0, 4]];
 assertEqual(JSON.stringify(expect), JSON.stringify(actual), desc);
 
 desc = "three pairs of integers";
-actual = merge([[4,5],[1,4],[0,1]]);
-expect = [[0,5]];
+actual = merge([
+  [4, 5],
+  [1, 4],
+  [0, 1],
+]);
+expect = [[0, 5]];
 assertEqual(JSON.stringify(expect), JSON.stringify(actual), desc);
 
 desc = "four pairs of integers";
-actual = merge([[1,3],[2,6],[8,10],[15,18]]);
-expect = [[1,6],[8,10],[15,18]];
+actual = merge([
+  [1, 3],
+  [2, 6],
+  [8, 10],
+  [15, 18],
+]);
+expect = [
+  [1, 6],
+  [8, 10],
+  [15, 18],
+];
 assertEqual(JSON.stringify(expect), JSON.stringify(actual), desc);
-
 
 function assertEqual(a, b, desc) {
   if (a === b) {
