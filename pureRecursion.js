db4b197c74ca77981d33f659f517a5b9d8400d5f
WinnieGotYu <WinnieGotYu@gmail.com>
2020-05-14 14:51:40 -0700

    added nestedEvenSum with recursion

--- pureRecursion.js
+++ pureRecursion.js
@@ -71,20 +71,56 @@
 // console.log(`[1,2,3,4] ... ${someRecursive([1,2,3,4], isOdd)} is true`);
 // console.log(`[4,6,8] ... ${someRecursive([4,6,8], isOdd)} is false`);
 
-function flattenArr(nestedArr) {
-  let flatten = [];
-  for (let ele of nestedArr) {
-    if (Array.isArray(ele)) {
-      flatten = flatten.concat(flattenArr(ele));
-    } else {
-      flatten.push(ele);
-    }
-  }
-  return flatten;
-}
+// function flattenArr(nestedArr) {
+//   let flatten = [];
+//   for (let ele of nestedArr) {
+//     if (Array.isArray(ele)) {
+//       flatten = flatten.concat(flattenArr(ele));
+//     } else {
+//       flatten.push(ele);
+//     }
+//   }
+//   return flatten;
+// }
 
-console.log(
-  `[[[[1], [[[2]]], [[[[[[[3]]]]]]]]]] ... ${flattenArr([
-    [[[[1], [[[2]]], [[[[[[[3]]]]]]]]]],
-  ])} is [1,2,3]`
-);
+// console.log(
+//   `[[[[1], [[[2]]], [[[[[[[3]]]]]]]]]] ... ${flattenArr([
+//     [[[[1], [[[2]]], [[[[[[[3]]]]]]]]]],
+//   ])} is [1,2,3]`
+// );
+
+// function nestedEvenSum(nestedObj){
+//   let sum = 0; 
+//   for(let key in nestedObj){
+//     if(typeof nestedObj[key] === 'object'){
+//       sum += nestedEvenSum(nestedObj[key]);
+//     } else if(typeof nestedObj[key] === 'number' && nestedObj[key] % 2 === 0){
+//       sum += nestedObj[key];
+//     }
+//   }
+//   return sum;
+// }
+
+// const obj2 = {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 2}, ee: 'car'}
+// };
+
+// const obj1 = {
+//   outer: 2,
+//   obj: {
+//     inner: 2,
+//     otherObj: {
+//       superInner: 2,
+//       notANumber: true,
+//       alsoNotANumber: "yup"
+//     }
+//   }
+// }
+
+// console.log(`obj1: ${nestedEvenSum(obj1)} is 6`);
+// console.log(`obj2: ${nestedEvenSum(obj2)} is 10`);
+
