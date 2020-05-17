c0fab855ed5f419a99f8f26486c115cd2e763606
WinnieGotYu <WinnieGotYu@gmail.com>
2020-05-17 12:24:00 -0700

    completed maxSwap' :

--- maxSwap.js
+++ maxSwap.js
@@ -13,39 +13,74 @@ Input: 9973
 Output: 9973
 Explanation: No swap. */
 
-function maxSwap(num){
-  const nums = num.toString().split('');
+// O(n^2) time 
+// function maxSwap(num){
+//   const nums = num.toString().split('');
 
-  for(let i = 0; i < nums.length; i++){
-    let currMaxNum = nums[i];
-    let index = i;
+//   for(let i = 0; i < nums.length; i++){
+//     let currMaxNum = nums[i];
+//     let index = i;
 
-    // find highest number
-    for(let j = i + 1; j < nums.length; j++){
-      if(nums[j] > currMaxNum){
-        currMaxNum = nums[j];
-        index = j;
+//     // find highest number
+//     for(let j = nums.length - 1; j > i; j--){
+//       if(nums[j] > currMaxNum){
+//         currMaxNum = nums[j];
+//         index = j;
+//       }
+//     }
+
+//     if(currMaxNum !== nums[i]){
+//       [nums[i], nums[index]] = [nums[index], nums[i]];
+//       return parseInt(nums.join(''));
+//     }
+//   }
+
+//   return num;
+// }
+
+
+// O(n) time and O(1) space 
+var maxSwap = function(num) {
+  const nums = num.toString();
+  var left = -1;
+  var right = -1;
+  var max = nums.length - 1; 
+  for (var i = nums.length - 2; i >= 0; i--) {
+      if (nums[i] < nums[max]) {
+          right = max;
+          left = i;
+      } else if (nums[i] > nums[max]) {
+          max = i;
       }
-    }
-
-    if(currMaxNum !== nums[i]){
-      [nums[i], nums[index]] = [nums[index], nums[i]];
-      return parseInt(nums.join(''));
-    }
   }
 
-  return num;
-}
+  if (left === -1) {
+      return Number(nums);
+  } else {
+      return swap(left, right, nums); 
+  }  
+};
 
-// TEST 
+var swap = function(min, max, nums) {
+  nums = nums.split('');
+  
+  var temp = nums[min];
+  nums[min] = nums[max];
+  nums[max] = temp;
+
+  return parseInt(nums.join(''));
+};
+// TEST
+
+function test(input, a, b) {
+  a === b
+    ? console.log(`${input}: PASS`)
+    : console.log(`${input}: FAIL, ${a} != ${b}`);
+}
 
 test('12390', maxSwap(12390), 92310);
 test('1100', maxSwap(1100), 1100);
-
-function test(input,a,b){
-  a === b
-  ? console.log(`${input}: PASS`)
-  : console.log(`${input}: FAIL, ${a} != ${b}`);
-}
-
-
+test("1993", maxSwap(1993), 9913);
+test(12, maxSwap(12), 21);
+test(10, maxSwap(10), 10);
+test(9973, maxSwap(9973), 9973);
