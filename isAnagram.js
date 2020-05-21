5b67d10389290ca0e3aa4cd19d17d98735c21df2
WinnieGotYu <WinnieGotYu@gmail.com>
2020-05-20 21:08:06 -0700

    added isAnagram with O(n)

---
+++ isAnagram.js
@@ -0,0 +1,26 @@
+function isAnagram(str, str2){
+  if(str.length !== str2.length) return false;
+
+  let seenChars = {}; 
+
+  for(let char of str){
+    seenChars[char] = seenChars[char]
+    ? seenChars[char] + 1
+    : 1; 
+  }
+
+  for(let char of str2){
+    if(!seenChars[char]){
+      return false;
+    } else if(seenChars[char] <= 0){
+      return false;
+    } else {
+      seenChars[char]--;
+    }
+  }
+
+  return true;
+}
+
+console.log(`anagram and nagaram: true = ${isAnagram("anagram", "nagaram")}`);
+console.log(`a and b: false = ${isAnagram('a', 'b')}`);
