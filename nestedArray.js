52ca4bfb8a6cd49ab4ec735822cb892708b3ab5c
Winnie Yu <winnie@winnie.local>
2020-07-16 14:06:09 -0700

    nestedArray.js WIP

---
+++ nestedArray.js
@@ -0,0 +1,9 @@
+/** 
+Given a nested array, return an array of all possible combinations.
+Constraints: - length of array is not provided
+             - inner array sizes can vary
+             - elements in array will be random string or number 
+Example: 
+array = [[a,b],[+,-],[1,2,3]]  
+output = ["a+1", "a+2", "a+3", "a-1", "a-2", "a-2"... "b+1", "b+2", "b+3", "b-1"]
+*/
