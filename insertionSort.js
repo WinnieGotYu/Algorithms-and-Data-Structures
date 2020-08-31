3b864a0cd4b110d405a67a798ed129c8fb3a2a4d
winniegotyu <winniegotyu@gmail.com>
2020-08-31 11:58:44 -0700

    Added insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+const insertionSort = (arr) => {
+  // start index at 1, assume element at 
+  //index 0 is in the correct position
+  for(let i = 1; i < arr.length; i++){
+    let j = i - 1;
+    let current = arr[i];
+    while(j >= 0 && arr[j] > current){
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = current;
+  }
+  return arr;
+}
