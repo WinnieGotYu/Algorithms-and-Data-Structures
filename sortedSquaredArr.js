570f0b02a084df3716507a9124e24a0ba32c0473
winniegotyu <winniegotyu@gmail.com>
2020-08-31 16:36:11 -0700

    Refactorted

--- sortedSquaredArr.js
+++ sortedSquaredArr.js
@@ -1,17 +1,16 @@
 function sortedSquareArr(nums){
   let left = 0; 
   let right = nums.length - 1; 
-  const result = new Array(nums.length);
-  for(let i = nums.length - 1; i > 0; i--){
+  let result = [];
+  for(let i = nums.length - 1; i >= 0; i--){
     if(Math.abs(nums[left]) > nums[right]){
-      result[i] = nums[left] * nums[left];
+      result[i] = nums[left] ** 2;
       left++;
     } else { 
-      result[i] = nums[right] * nums[right];
+      result[i] = nums[right] ** 2;
       right--;
     }
   }
   return result;
 }
-
-console.log(`[-6,-4,1,2,3,5]: ${sortedSquareArr([-6,-4, ,1,2,3,5])} = [1,2,9,16,25,36]`)
+console.log(`[${sortedSquareArr([-6,-4,1,2,3,5])}] = [1,4,9,16,25,36]`)
