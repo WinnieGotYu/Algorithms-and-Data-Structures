5973fb3e4be4f37a8968d48a356d99c8fbe5167c
winniegotyu <winniegotyu@gmail.com>
2020-09-04 16:09:52 -0700

    Refactored

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -14,19 +14,30 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 //using kadane's algorithm 
 
-var maxSubArray = function(nums) {
-  let currSum = -Infinity,
-      max = -Infinity;
+// var maxSubArray = function(nums) {
+//   let currSum = -Infinity,
+//       max = -Infinity;
 
-  for(let i in nums){
-    //compares current number to current sum plus current num , keeps larger of the two
-    //checks if num is negative
-    currSum = Math.max(nums[i], currSum + nums[i]);
-    //checks if new sum is greater than max sum 
-    max = Math.max(max, currSum);
+//   for(let i in nums){
+//     //compares current number to current sum plus current num , keeps larger of the two
+//     //checks if num is negative
+//     currSum = Math.max(nums[i], currSum + nums[i]);
+//     //checks if new sum is greater than max sum 
+//     max = Math.max(max, currSum);
+//   }
+
+//   return max; 
+// };
+
+const maxSubArray = (arr) => {
+  let currSum = -Infinity, 
+      maxSum = -Infinity;
+  for(let num of arr){
+    currSum = Math.max(num, currSum + num);
+    maxSum = Math.max(max, currSum);
   }
-
-  return max; 
-};
+  
+  return maxSum
+}
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); //6
