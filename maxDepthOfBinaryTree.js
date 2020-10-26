6e7ce4a1f689e6ef05780e4c712afb0cd4f98570
winniegotyu <winniegotyu@gmail.com>
2020-10-26 15:08:53 -0700

    Renamed helper function

--- maxDepthOfBinaryTree.js
+++ maxDepthOfBinaryTree.js
@@ -15,21 +15,29 @@ Given binary tree [3,9,20,null,null,15,7],
     /  \
    15   7
 return its depth = 3.
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * @param {TreeNode} root
+ * @return {number}
  */
 
 const maxDepth = function(root) {
   if(!root) return 0;
   let maxDepth = -Infinity;
   
-  const DFS = (currentNode, depth=1) => {
+  const traverse = (currentNode, depth=1) => {
       if(!currentNode.left && !currentNode.right){
           maxDepth = Math.max(maxDepth, depth);
           return;
       }
-      if(currentNode.left) DFS(currentNode.left, depth + 1);
-      if(currentNode.right) DFS(currentNode.right, depth + 1);
+      if(currentNode.left) traverse(currentNode.left, depth + 1);
+      if(currentNode.right) traverse(currentNode.right, depth + 1);
    }
   
-  DFS(root);
+  traverse(root);
   return maxDepth;
 };
